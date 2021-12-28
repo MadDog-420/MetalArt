f12b4a28b5bff3a6f61168400e8f9973391119c3
MadDog-420 <u18211928@utp.edu.pe>
2021-12-28 14:17:22 -0500

    build test 3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+    /* publicPath: process.env.NODE_ENV === 'production'
+      ? '/test/dist/'
+      : './' 
+    */
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/MetalArt/dist/'
-      : './'
+      ? '/MetalArt/'
+      : '/'
   }
